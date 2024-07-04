70cf2fabf28f6b3a029076122944a85bea1a7964
Andre <andre021297@gmail.com>
2024-07-04 08:59:38 -0500

    new repo esteban

--- src/controllers/alarmEvent/getAllAlarmsForPatientController.js
+++ src/controllers/alarmEvent/getAllAlarmsForPatientController.js
@@ -1,12 +1,20 @@
 import getAllAlarmsForPatientHandler from "../../handlers/alarmEvent/getAllAlarmsForPatientHandler.js";
+import getAlarmByIdHandler from "../../handlers/alarmEvent/getAlarmByIdHandler.js";
 
 const getAllAlarmsForPatientController = async (req, res) => {
   try {
-    const {patientId} = req.params;
+    const { patientId } = req.params;  // optional patientId
+    const { alarmId } = req.query;
 
-    const allAlarmsForPatient = await getAllAlarmsForPatientHandler(patientId);
-    
-    return res.status(200).json(allAlarmsForPatient);
+    if (alarmId) {
+      const alarm = await getAlarmByIdHandler(alarmId);
+      return res.status(200).json(alarm);
+    } else if (patientId) {
+      const allAlarmsForPatient = await getAllAlarmsForPatientHandler(patientId);
+      return res.status(200).json(allAlarmsForPatient);
+    } else {
+      return res.status(400).json({ error: "Debe proporcionar un patientId o un alarmId" });
+    }
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
