e7728daac204a45fb5ce97711899f0f91a7dda9f
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-07-05 20:58:57 -0300

    patients pagination: getPatiensController and getPatientsHandler modified

--- src/controllers/patient/getPatientsController.js
+++ src/controllers/patient/getPatientsController.js
@@ -1,12 +1,14 @@
 import getPatientsHandler from "../../handlers/patient/getPatientsHandler.js";
 
 const getPatientsController = async (req, res) => {
-    try {
-        const users = await getPatientsHandler();
-        return res.status(200).json(users);
-    } catch (error) {
-        return res.status(500).json({error: error.message});
-    }
+  const { limit, page, filter } = req.query;
+  console.log(limit, page);
+  try {
+    const users = await getPatientsHandler({ limit, page, filter });
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
 
 export default getPatientsController;
--- src/handlers/patient/getPatientsHandler.js
+++ src/handlers/patient/getPatientsHandler.js
@@ -1,16 +1,32 @@
-import models from '../../databaseConfig.js'
+import models from "../../databaseConfig.js";
 
-const getPatientsHandler = async () => {
+const getPatientsHandler = async ({ limit, page, filter }) => {
   try {
-    const getPatients = await models.User.findAll({
-      where:{
-        role:3
-      },
-      attributes: {
-        exclude: ['password','cellphone', 'email']
-      }
-    });
-    return getPatients;
+    if (!limit || !page) {
+      const getPatients = await models.User.findAll({
+        where: {
+          role: 3,
+        },
+        attributes: {
+          exclude: ["password", "cellphone", "email"],
+        },
+      });
+      return getPatients;
+    } else {
+      const offset = (page - 1) * limit;
+      const { count, rows: patients } = await models.User.findAndCountAll({
+        limit,
+        offset,
+      });
+      const totalPages = Math.ceil(count / limit);
+
+      return {
+        totalItems: count,
+        totalPages,
+        currentPage: page,
+        patients,
+      };
+    }
   } catch (error) {
     throw new Error("Error loading patients: " + error.message);
   }
