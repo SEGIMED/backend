77d134dcead9a1f282f414205d9b89c02424b882
Hernan Parino <you@example.com>
2024-07-10 14:25:39 -0300

    se agrega ruta/controler/handler getdeathRate. Se modifica modelo SociodemograficDetails agrgo columna dia de registro y coloco default null a dia de reporte de la muerte

---
+++ src/handlers/statisticalCenter/getDeathRateHandler.js
@@ -0,0 +1,39 @@
+import { SociodemographicDetails } from "../../databaseConfig.js";
+import { Op } from "sequelize";
+import SegimedAPIError from "../../error/SegimedAPIError.js";
+
+const getDeathRateHandler = async (from, to) => {
+    
+    try {
+        const deathCounter = await SociodemographicDetails.count({
+            where: {
+                dateOfDeathReport: {
+                    [Op.between]: [from, to],
+                },
+            },
+        });// con esto obtengo los muertos entre esas fechas
+        
+        // cantidad de pacientes activon entre (from,to) = registrados hasta to - muertos hasta from
+        const registredInDateCount = await SociodemographicDetails.count({
+            where: {
+                registrationDate: {
+                    [Op.lte]: to,
+                },
+            },
+        });// cuento los registrados hasta la fecha TO
+
+        const historyDeathCount = await SociodemographicDetails.count({
+            where: {
+                dateOfDeathReport: {
+                    [Op.lte]: from,
+                },
+            },
+        });// cuento los muertos historicos hasta la fecha FROM
+        console.log("regi, histor",registredInDateCount, historyDeathCount);
+        return { from, to, dead: deathCounter, alive: (registredInDateCount - historyDeathCount) };
+    } catch (error) {
+        throw new SegimedAPIError("Error al cargar las estadísticas de mortalidad: ", 500);
+    }
+};
+
+export default getDeathRateHandler;
