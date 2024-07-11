d559182e6fc17e3700580351fbcf11718d26ecb1
Maria Gabriela Sándigo Muñoz <137342274+MGSandigoMunoz@users.noreply.github.com>
2024-07-11 17:25:34 -0500

    Edited: povisionalPreConsultation. Parse added to studies Cloudinary. Function loadFile created to implement in preconsultation

--- src/mapper/painMap/painMapMapper.js
+++ src/mapper/painMap/painMapMapper.js
@@ -1,8 +1,9 @@
 export const mapPainMap = (painMap) => {
+    
     if (!painMap) {
         throw new Error('Pain map object is required.');
     }
-
+     
     const mappedPainMap = {
         isTherePain: painMap.isTherePain || false,
         isTakingAnalgesic: painMap.isTakingAnalgesic || false,
@@ -35,136 +36,50 @@ const mapPainAreas = (painAreas) => {
     }));
 };
 const painAreaNameMap = (painArea) => {
-    if (painArea == 1) {
-        return {
-            painAreaEnglish: "trapezius",
-            painAreaSpanish: "Trapecio"
-        }
+    switch(painArea) {
+        case 1:
+            return { painAreaEnglish: "trapezius", painAreaSpanish: "Trapecio" };
+        case 2:
+            return { painAreaEnglish: "upper-back", painAreaSpanish: "Espalda Superior" };
+        case 3:
+            return { painAreaEnglish: "lower-back", painAreaSpanish: "Espalda Inferior" };
+        case 4:
+            return { painAreaEnglish: "chest", painAreaSpanish: "Pecho" };
+        case 5:
+            return { painAreaEnglish: "biceps", painAreaSpanish: "Bíceps" };
+        case 6:
+            return { painAreaEnglish: "triceps", painAreaSpanish: "Tríceps" };
+        case 7:
+            return { painAreaEnglish: "forearm", painAreaSpanish: "Antebrazo" };
+        case 8:
+            return { painAreaEnglish: "back-deltoids", painAreaSpanish: "Deltoides Posterior" };
+        case 9:
+            return { painAreaEnglish: "front-deltoids", painAreaSpanish: "Deltoides Anterior" };
+        case 10:
+            return { painAreaEnglish: "abs", painAreaSpanish: "Abdominales" };
+        case 11:
+            return { painAreaEnglish: "obliques", painAreaSpanish: "Oblicuos" };
+        case 12:
+            return { painAreaEnglish: "adductor", painAreaSpanish: "Aductores" };
+        case 13:
+            return { painAreaEnglish: "hamstring", painAreaSpanish: "Isquiotibiales" };
+        case 14:
+            return { painAreaEnglish: "quadriceps", painAreaSpanish: "Cuádriceps" };
+        case 15:
+            return { painAreaEnglish: "abductors", painAreaSpanish: "Abductores" };
+        case 16:
+            return { painAreaEnglish: "calves", painAreaSpanish: "Pantorrillas" };
+        case 17:
+            return { painAreaEnglish: "gluteal", painAreaSpanish: "Glúteos" };
+        case 18:
+            return { painAreaEnglish: "knees", painAreaSpanish: "Rodillas" };
+        case 21:
+            return { painAreaEnglish: "head", painAreaSpanish: "Cabeza" };
+        case 22:
+            return { painAreaEnglish: "neck", painAreaSpanish: "Cuello" };
+        default:
+            return { painAreaEnglish: "", painAreaSpanish: "" };
     }
-    if (painArea == 2) {
-        return {
-            painAreaEnglish: "upper-back",
-            painAreaSpanish: "Espalda Superior"
-        }
-    }
-    if (painArea == 3) {
-        return {
-            painAreaEnglish: "lower-back",
-            painAreaSpanish: "Espalda Inferior"
-        }
-    }
-    if (painArea == 4) {
-        return {
-            painAreaEnglish: "chest",
-            painAreaSpanish: "Pecho"
-        }
-    }
-    if (painArea == 5) {
-        return {
-            painAreaEnglish: "biceps",
-            painAreaSpanish: "Bíceps"
-        }
-    }
-    if (painArea == 6) {
-        return {
-            painAreaEnglish: "triceps",
-            painAreaSpanish: "Tríceps"
-        }
-    }
-    if (painArea == 7) {
-        return {
-            painAreaEnglish: "forearm",
-            painAreaSpanish: "Antebrazo"
-        }
-    }
-    if (painArea == 8) {
-        return {
-            painAreaEnglish: "back-deltoids",
-            painAreaSpanish: "Deltoides Posterior"
-        }
-    }
-    if (painArea == 9) {
-        return {
-            painAreaEnglish: "front-deltoids",
-            painAreaSpanish: "Deltoides Anterior"
-        }
-    }
-    if (painArea == 10) {
-        return {
-            painAreaEnglish: "abs",
-            painAreaSpanish: "Abdominales"
-        }
-    }
-    if (painArea == 11) {
-        return {
-            painAreaEnglish: "obliques",
-            painAreaSpanish: "Oblicuos"
-        }
-    }
-    if (painArea == 12) {
-        return {
-            painAreaEnglish: "adductor",
-            painAreaSpanish: "Aductores"
-        }
-    }
-    if (painArea == 13) {
-        return {
-            painAreaEnglish: "hamstring",
-            painAreaSpanish: "Isquiotibiales"
-        }
-    }
-    if (painArea == 14) {
-        return {
-            painAreaEnglish: "quadriceps",
-            painAreaSpanish: "Cuádriceps"
-        }
-    }
-    if (painArea == 15) {
-        return {
-            painAreaEnglish: "abductors",
-            painAreaSpanish: "Abductores"
-        }
-    }
-    if (painArea == 16) {
-        return {
-            painAreaEnglish: "calves",
-            painAreaSpanish: "Pantorrillas"
-        }
-    }
-    if (painArea == 17) {
-        return {
-            painAreaEnglish: "gluteal",
-            painAreaSpanish: "Glúteos"
-        }
-    }
-    if (painArea == 18) {
-        return {
-            painAreaEnglish: "knees",
-            painAreaSpanish: "Rodillas"
-        }
-    }
-    if (painArea == 19) {
-        return {
-            painAreaEnglish: "right-soleus",
-            painAreaSpanish: "Sóleo Derecho"
-        }
-    }
-    if (painArea == 20) {
-        return {
-            painAreaEnglish: "left-soleus",
-            painAreaSpanish: "Sóleo Izquierdo"
-        }
-    }
-    if (painArea == 21) {
-        return {
-            painAreaEnglish: "head",
-            painAreaSpanish: "Cabeza"
-        }
-    }
-    if (painArea == 22) {
-        return {
-            painAreaEnglish: "neck",
-            painAreaSpanish: "Cuello"
-        }
-    }
 }
+
+
