7626c4e9befcdc8caf42d3cceac5b9fe60b442e3
gablon29 <gabriellondero40@gmail.com>
2024-07-11 19:07:07 -0300

    configurando jest en la aplicacion para hacer los test

--- src/controllers/managementSchedule/getScheduleId.js
+++ src/controllers/managementSchedule/getScheduleId.js
@@ -1,13 +1,12 @@
-import getScheduleByIdHandler from "../../handlers/managementSchedule/getScheduleByIdHandler";
-
+import getScheduleByIdHandler from "../../handlers/managementSchedule/getScheduleByIdHandler.js";
 
 const getScheduleById = async (req, res) => {
-    const { id } = req.params;
-    try {
-        const response = await getScheduleByIdHandler(id);
-        res.status(200).send(response);
-    } catch (error) {
-        res.status(404).json({ msj: error.message })
-    }
-}
+  const { id } = req.params;
+  try {
+    const response = await getScheduleByIdHandler(id);
+    res.status(200).send(response);
+  } catch (error) {
+    res.status(404).json({ msj: error.message });
+  }
+};
 export default getScheduleById;
