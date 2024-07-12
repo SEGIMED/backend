7a6a35cc511b70132add61bbef747c474a64d498
gabri <gabriellondero40@gmail.com>
2024-07-12 05:25:38 -0300

    subiendo cambios local

---
+++ demo.js
@@ -0,0 +1,3 @@
+const date = new Date("23:00:00");
+const hora = date.getTime().toFixed();
+console.log(hora);
--- src/models/DoctorSchedule.js
+++ src/models/DoctorSchedule.js
@@ -3,50 +3,43 @@ import sequelize from "sequelize";
 const DataTypes = sequelize.DataTypes;
 
 const model = (sequelize) => {
-    sequelize.define("DoctorSchedule", {
-        id: {
-            autoIncrement: true,
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false
-        }, 
-        doctor_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: "User", // foreinkey
-                key: "id" // clave de relacion
+  sequelize.define(
+    "DoctorSchedule",
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      doctor_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "User", // foreinkey
+          key: "id", // clave de relacion
         },
-            onUpdate: "CASCADE",
-        },
-        date: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
-        },
-        start_time: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        end_time: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
+      },
+      date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      start_time: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      end_time: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
     },
-        updatedAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
+    {
+      tableName: "doctor_schedule",
+      schema: "public",
+      timesTamps: false,
     }
-    }, {
-        tableName: "doctor_schedule",
-        schema: "public",
-        timesTamps: true
-    })
-}
+  );
+};
 
 export default model;
