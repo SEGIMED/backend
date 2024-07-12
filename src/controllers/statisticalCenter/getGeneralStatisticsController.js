55eebffd500d47c18b2a90731b77e0c700eee66e
Hernan Parino <you@example.com>
2024-07-11 21:51:34 -0300

    se integran todas las estadisticas a la misma ruta statisticsGeneralse elimina ruta deaths, se agregan estadisticas de alarmas

--- src/controllers/statisticalCenter/getGeneralStatisticsController.js
+++ src/controllers/statisticalCenter/getGeneralStatisticsController.js
@@ -6,22 +6,26 @@ import getAlarmEventsStatisticsHandler from '../../handlers/statisticalCenter/ge
 
 const getGeneralStatisticsController = async (req, res) => {
     try {
-        let actual_year = {
+        const actual_year = {
             from : moment().startOf('year').format('YYYY-MM-DD'),
             to : moment().endOf('year').format('YYYY-MM-DD'),
         }
-        let actual_month = {
+        const actual_month = {
             from : moment().startOf('month').format('YYYY-MM-DD'),
             to : moment().endOf('month').format('YYYY-MM-DD'),
         }
+        const last24hs = {
+            from: moment().subtract(24, 'hours').format('YYYY-MM-DD HH:mm:ss'),
+            to: moment().format('YYYY-MM-DD HH:mm:ss'),
+        };
 
         const yearDeathRateStatistics = await getDeathRateHandler(actual_year.from, actual_year.to);
         const monthDeathRateStatistics = await getDeathRateHandler(actual_month.from, actual_month.to);
         const genderStatistics = await getGenderDistributionHandler();
         const patientActivityDistribution = await getPatientActivityDistributionHandler();
-        const yearAlarmEventsStatistics = await getAlarmEventsStatisticsHandler(actual_year.from, actual_year.to)
-        const monthAlarmEventsStatistics = await getAlarmEventsStatisticsHandler(actual_month.from, actual_month.to);
-        const statistics = {yearAlarmEventsStatistics, monthAlarmEventsStatistics, yearDeathRateStatistics, monthDeathRateStatistics, genderStatistics, patientActivityDistribution};
+        const alarmEventsStatistics = await getAlarmEventsStatisticsHandler("0001-01-01", actual_year.to);
+        const last24hsAlarmStatistics = await getAlarmEventsStatisticsHandler(last24hs.from, last24hs.to);
+        const statistics = {alarmEventsStatistics, last24hsAlarmStatistics, yearDeathRateStatistics, monthDeathRateStatistics, genderStatistics, patientActivityDistribution};
 
         return res.status(200).json(statistics);
     } catch (error) {
