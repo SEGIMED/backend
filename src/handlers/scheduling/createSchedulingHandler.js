f48f22af52fe058ac3802801bddff66e675cad07
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-07-14 16:06:53 -0300

    getAllProvisionaPreConsultationPatientHandler modified

--- src/handlers/scheduling/createSchedulingHandler.js
+++ src/handlers/scheduling/createSchedulingHandler.js
@@ -1,19 +1,13 @@
-import {AppointmentScheduling} from "../../databaseConfig.js";
+import { AppointmentScheduling } from "../../databaseConfig.js";
 import SegimedAPIError from "../../error/SegimedAPIError.js";
 
-
 const createSchedulingHandler = async (body) => {
-    try {
-        const newScheduling = await AppointmentScheduling.create(
-            body
-        )
-        
-        return newScheduling
-    } catch (error) {
-        console.log(error)
-        throw new SegimedAPIError("Error al crear el agendamiento", 500)
-    }
-
-}
+  try {
+    const newScheduling = await AppointmentScheduling.create(body);
+    return newScheduling;
+  } catch (error) {
+    throw new SegimedAPIError("Error al crear el agendamiento", 500);
+  }
+};
 
 export default createSchedulingHandler;
