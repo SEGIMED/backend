e268b380f939c0023c3c0b32342e9695a69fad80
Hernan Parino <you@example.com>
2024-07-17 00:13:07 -0300

    FIX: ruta get fav by physicianID, se muestran los datos que el front requiere, tal como la ruta getPatientes

--- src/controllers/patient/getPatientsController.js
+++ src/controllers/patient/getPatientsController.js
@@ -2,6 +2,7 @@ import getPatientsHandler from "../../handlers/patient/getPatientsHandler.js";
 
 const getPatientsController = async (req, res) => {
   const { limit, page, name, lastname, idNumber } = req.query;
+  console.log( limit, page, name, lastname, idNumber );
   try {
     const users = await getPatientsHandler({ limit, page, name, lastname, idNumber });
     return res.status(200).json(users);
