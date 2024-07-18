4a3f47c6db072c84e833983d2ba18c5fef910d96
gabri <gabriellondero40@gmail.com>
2024-07-18 07:02:58 -0300

    feat: Add createOnbording controller

---
+++ src/controllers/onbording/createOnbording.js
@@ -0,0 +1,11 @@
+import createOnbordingHandler from "../../handlers/onbording/createOnbordingHandler";
+
+export const createOnbording = async (req, res) => {
+  try {
+    const newOnbording = req.body;
+    const onbording = await createOnbordingHandler(newOnbording);
+    return res.status(200).json(onbording);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
