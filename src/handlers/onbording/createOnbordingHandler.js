6c35f1349773e2570437da3a28dc077e58817fe6
gablon29 <gabriellondero40@gmail.com>
2024-07-18 10:36:25 -0300

    Refactor createOnbording handler to handle error and return updated data

--- src/handlers/onbording/createOnbordingHandler.js
+++ src/handlers/onbording/createOnbordingHandler.js
@@ -1,28 +1,37 @@
-import { SociodemographicDetails } from "../../databaseConfig";
+import { SociodemographicDetails } from "../../databaseConfig.js";
+import SegimedAPIError from "../../error/SegimedAPIError.js";
 
 export const createOnbordingHandler = async (body) => {
-  const {
-    userId,
-    hipertPulm,
-    centerAttention,
-    liveAlone,
-    hasTechUseDifficulty,
-    needsCellphoneAssistance,
-  } = body;
-  const data = await SociodemographicDetails.update(
-    {
+  try {
+    const {
+      userId,
       hipertPulm,
       centerAttention,
       liveAlone,
       hasTechUseDifficulty,
       needsCellphoneAssistance,
-    },
-    {
-      where: {
-        patient: userId,
+    } = body;
+    const data = await SociodemographicDetails.update(
+      {
+        hipertPulm,
+        centerAttention,
+        liveAlone,
+        hasTechUseDifficulty,
+        needsCellphoneAssistance,
       },
-    }
-  );
-  return data;
+      {
+        where: {
+          patient: userId,
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    throw new SegimedAPIError(
+      "Error en la operacion de registro: ",
+      error,
+      500
+    );
+  }
 };
 export default createOnbordingHandler;
