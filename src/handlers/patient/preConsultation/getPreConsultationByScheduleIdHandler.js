cd79b5240755085c2a6104342337fe8639860b8d
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-07-21 13:29:48 -0300

    getPreConsultationBySvheduleIdHandler modified: find the error

--- src/handlers/patient/preConsultation/getPreConsultationByScheduleIdHandler.js
+++ src/handlers/patient/preConsultation/getPreConsultationByScheduleIdHandler.js
@@ -3,110 +3,108 @@ import SegimedAPIError from "../../../error/SegimedAPIError.js";
 import { consultationVitalSignsMapper } from "../../../mapper/patient/consultationVitalSignsMapper.js";
 
 const getPreConsultationByScheduleIdHandler = async (scheduleId) => {
-    try {
-        const preConsultations = await models.ProvisionalPreConsultation.findAll(
-          {
-            where: {
-                appointmentSchedule:scheduleId,
+  try {
+    const preConsultations = await models.ProvisionalPreConsultation.findAll({
+      where: {
+        appointmentSchedule: scheduleId,
+      },
+      include: [
+        {
+          model: models.PatientPainMap,
+          as: "provisionalPreConsultationPainMap",
+          include: [
+            {
+              model: models.CatPainDuration,
+              as: "catPainDuration",
             },
-            include: [
-              {
-                model: models.PatientPainMap,
-                as: "provisionalPreConsultationPainMap",
-                include: [
-                  {
-                    model: models.CatPainDuration,
-                    as: "catPainDuration",
-                  },
-                  {
-                    model: models.CatPainScale,
-                    as: "catPainScale",
-                  },
-                  {
-                    model: models.CatPainType,
-                    as: "catPainType",
-                  },
-                  {
-                    model: models.CatPainFrequency,
-                    as: "catPainFrequency",
-                  },
-                  {
-                    model: models.User,
-                    as: "painRecorderUser",
-                  },
-                ],
-              },
-              {
-                model: models.AppointmentScheduling,
-                as: "ProvisionalPreConsultationSchedule",
-                include: [
-                  {
-                    model: models.VitalSignDetails,
-                    as: "vitalSignDetailsScheduling",
-                    //Indicate to make separate request for each model
-                    separate: true,
-                    include: [
-                      {
-                        model: models.CatVitalSignMeasureType,
-                        as: "vitalSignMeasureType",
-                        include: [
-                          {
-                            model: models.CatMeasureUnit,
-                            as: "measUnit",
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                  {
-                    model: models.User,
-                    as: "patientUser",
-                    attributes: ["name", "lastname", "id"],
-                  },
-                  {
-                    model: models.User,
-                    as: "physicianThatAttend",
-                    attributes: ["name", "lastname", "id"],
-                  },
-                  {
-                    model: models.CatMedicalSpecialty,
-                    as: "specialty",
-                  },
-                  {
-                    model: models.CatSchedulingStatus,
-                    as: "status",
-                  },
-                  {
-                    model: models.CatAppointmentModality,
-                    as: "appointmentModality",
-                  },
-                  {
-                    model: models.PhysicianAttendancePlace,
-                    as: "attendancePlace",
-                  },
-                ],
-              },
-            ],
-          }
-        );
-        if (preConsultations.length === 0) {
-          throw new SegimedAPIError("No se encontraron preconsultas para el ID de programación proporcionado.", 404);
-        }
-    
-        //! No está agregando los datos al objeto principal por eso lo mando en uno aparte
-        //! Realmente no puedo identificar el error
-        const preConsultation = preConsultations[0];
-        const vitalSignDetails = preConsultation.ProvisionalPreConsultationSchedule.vitalSignDetailsScheduling;
-       
-        const filteredVitalSigns = await consultationVitalSignsMapper(vitalSignDetails);
-        preConsultation["ProvisionalPreConsultationSchedule"]["vitalSignDetailsScheduling"] = filteredVitalSigns;
-        return {preConsultation,filteredVitalSigns};
-      } catch (error) {
-        throw new SegimedAPIError(
-          "Error al cargar los detalles de la preconsulta ",
-          500
-        );
-      }
+            {
+              model: models.CatPainScale,
+              as: "catPainScale",
+            },
+            {
+              model: models.CatPainType,
+              as: "catPainType",
+            },
+            {
+              model: models.CatPainFrequency,
+              as: "catPainFrequency",
+            },
+            {
+              model: models.User,
+              as: "painRecorderUser",
+            },
+          ],
+        },
+        {
+          model: models.AppointmentScheduling,
+          as: "ProvisionalPreConsultationSchedule",
+          include: [
+            {
+              model: models.VitalSignDetails,
+              as: "vitalSignDetailsScheduling",
+              separate: true,
+              include: [
+                {
+                  model: models.CatVitalSignMeasureType,
+                  as: "vitalSignMeasureType",
+                  include: [
+                    {
+                      model: models.CatMeasureUnit,
+                      as: "measUnit",
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              model: models.User,
+              as: "patientUser",
+              attributes: ["name", "lastname", "id"],
+            },
+            {
+              model: models.User,
+              as: "physicianThatAttend",
+              attributes: ["name", "lastname", "id"],
+            },
+            {
+              model: models.CatMedicalSpecialty,
+              as: "specialty",
+            },
+            {
+              model: models.CatSchedulingStatus,
+              as: "status",
+            },
+            {
+              model: models.CatAppointmentModality,
+              as: "appointmentModality",
+            },
+            {
+              model: models.PhysicianAttendancePlace,
+              as: "attendancePlace",
+            },
+          ],
+        },
+      ],
+    });
+
+    if (preConsultations.length === 0) {
+      throw new SegimedAPIError("No se encontraron preconsultas para el ID de programación proporcionado.", 404);
+    }
+
+    const preConsultation = preConsultations[0].get({ plain: true }); //Era lo que faltaba
+    const vitalSignDetails = preConsultation.ProvisionalPreConsultationSchedule.vitalSignDetailsScheduling;
+
+    const filteredVitalSigns = await consultationVitalSignsMapper(vitalSignDetails);
+
+    preConsultation.ProvisionalPreConsultationSchedule.vitalSignDetailsScheduling = filteredVitalSigns;
+
+    return preConsultation;
+  } catch (error) {
+    throw new SegimedAPIError(
+      "Error al cargar los detalles de la preconsulta ",
+      500
+    );
+  }
 };
 
 export default getPreConsultationByScheduleIdHandler;
