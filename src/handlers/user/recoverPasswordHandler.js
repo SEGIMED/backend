36981832023caf8545651158392b45ed8b68826c
Andre <andre021297@gmail.com>
2024-07-25 14:24:47 -0500

    add recover Password template HTML

--- src/handlers/user/recoverPasswordHandler.js
+++ src/handlers/user/recoverPasswordHandler.js
@@ -1,42 +1,41 @@
 import SegimedAPIError from "../../error/SegimedAPIError.js";
-import {sequelize, User} from "../../databaseConfig.js";
+import { sequelize, User } from "../../databaseConfig.js";
 import SegimedInputValidationError from "../../error/SegimedInputValidationError.js";
-import {generateOTP} from "../../utils/generateOTP.js";
-import {sendMail} from "../../utils/sendMail.js";
-
+import { generateOTP } from "../../utils/generateOTP.js";
+import { sendMail } from "../../utils/sendMail.js";
+import recoverPasswordHtml from "../../utils/emailTemplates/recoverPasswordHtml.js";
 
 const recoverPasswordHandler = async (body) => {
-    const {email} = body
+  const { email } = body;
 
+  let databaseUser;
+  try {
+    databaseUser = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    throw new SegimedAPIError("Hubo un error procesando la solicitud", 500);
+  }
 
-    let databaseUser
-    try {
-        databaseUser = await User.findOne({
-            where: {
-                email: email
-            }
-        })
-    } catch (error) {
-        console.error(error);
-        throw new SegimedAPIError("Hubo un error procesando la solicitud", 500)
-    }
+  if (!databaseUser)
+    throw new SegimedInputValidationError("Este correo no existe.");
 
-    if (!databaseUser)
-        throw new SegimedInputValidationError('Este correo no existe.');
+  await sequelize.transaction(async (t) => {
+    const userOtp = await generateOTP(databaseUser);
+    const emailSent = recoverPasswordHtml(userOtp);
+    await sendMail(
+      databaseUser.email,
+      emailSent,
+      "Tu código para recuperar tu contraseña"
+    );
+  });
 
-    await sequelize.transaction(async t => {
-        const userOtp = await generateOTP(databaseUser)
-        await sendMail(databaseUser.email, userOtp, "OTP para recuperar tu clave")
-    })
-
-    const cleanUser = databaseUser.dataValues
-    delete cleanUser.password
-    return cleanUser
-
-}
+  const cleanUser = databaseUser.dataValues;
+  delete cleanUser.password;
+  return cleanUser;
+};
 
 export default recoverPasswordHandler;
-
-
-
-
