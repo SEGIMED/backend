3725c3cb740d37cfdebbdf18d292348c37cb9e3c
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-07-31 15:46:15 -0300

    if added

--- src/handlers/patient/getPatientsHandler.js
+++ src/handlers/patient/getPatientsHandler.js
@@ -1,26 +1,28 @@
 import models, { PhysicianFavoritePatient } from "../../databaseConfig.js";
 import paginationUsersHandler from "../Pagination/paginationUsersHandler.js";
-import { mapPatients } from '../../mapper/patient/patientMapper.js';
+import { mapPatients } from "../../mapper/patient/patientMapper.js";
 import { Op } from "sequelize";
 
-const getPatientsHandler = async ({ limit, page, name, risk, physicianId, treating }, onlyFavorites) => {
+const getPatientsHandler = async (
+  { limit, page, name, risk, physicianId, treating },
+  onlyFavorites
+) => {
   try {
     const queryOptions = {
       where: {
         role: 3,
-        treatingPhysician: treating ? physicianId : undefined
-      },  
+      },
       attributes: {
         exclude: ["password", "cellphone", "email"],
       },
       include: [
         {
           model: models.PatientPulmonaryHypertensionRisk,
-          as: 'patientPulmonaryHypertensionRisks',
+          as: "patientPulmonaryHypertensionRisks",
           include: {
             model: models.CatPulmonaryArterialHypertensionRisk,
-            as: 'catHpRisk',
-            attributes: ['name'],
+            as: "catHpRisk",
+            attributes: ["name"],
             ...(risk ? { where: { name: { [Op.iLike]: `%${risk}%` } } } : {}),
           },
         },
@@ -30,22 +32,22 @@ const getPatientsHandler = async ({ limit, page, name, risk, physicianId, treati
           as: "favorites",
           attributes: ["physicianId", "favoritePatient"],
           where: {
-            physicianId: physicianId
+            physicianId: physicianId,
           },
-          required: false  // Allow for patients who are not favorites
-        }
-      ].filter(Boolean) // Elimina cualquier valor false de la lista de includes
+          required: false, // Allow for patients who are not favorites
+        },
+      ].filter(Boolean), // Elimina cualquier valor false de la lista de includes
     };
 
     // Agregar filtros de búsqueda
     if (name) {
-      const searchTerms = name.split(' ').filter(term => term.trim() !== '');
-      queryOptions.where[Op.or] = searchTerms.map(term => ({
+      const searchTerms = name.split(" ").filter((term) => term.trim() !== "");
+      queryOptions.where[Op.or] = searchTerms.map((term) => ({
         [Op.or]: [
           { name: { [Op.iLike]: `%${term}%` } },
           { lastname: { [Op.iLike]: `%${term}%` } },
-          { idNumber: { [Op.iLike]: `%${term}%` } }
-        ]
+          { idNumber: { [Op.iLike]: `%${term}%` } },
+        ],
       }));
     }
 
@@ -56,19 +58,26 @@ const getPatientsHandler = async ({ limit, page, name, risk, physicianId, treati
         as: "favorites",
         attributes: ["physicianId", "favoritePatient"],
         where: {
-          physicianId: physicianId
+          physicianId: physicianId,
         },
-        required: true // Asegura que solo se incluyan pacientes que son favoritos
+        required: true, // Asegura que solo se incluyan pacientes que son favoritos
       });
+    }
+
+    if (physicianId && treating) {
+      queryOptions.where = {
+        role: 3,
+        treatingPhysician: physicianId,
+      };
     }
 
     // Sin paginación
     if (!limit && !page) {
       const getPatients = await models.User.findAll(queryOptions);
-      return mapPatients(getPatients );
+      return mapPatients(getPatients);
     } else {
       // Lógica de paginación
-      return paginationUsersHandler({ page, limit, queryOptions } );
+      return paginationUsersHandler({ page, limit, queryOptions });
     }
   } catch (error) {
     throw new Error("Error loading patients: " + error.message);
@@ -114,7 +123,7 @@ export default getPatientsHandler;
 //         where: {
 //           physicianId: physicianId
 //         },
-//         required: false  
+//         required: false
 //       });
 //     }
 
