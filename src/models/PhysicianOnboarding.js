b0318dd0cd73791747b35ae89cb622862f9ad592
gablon29 <gabriellondero40@gmail.com>
2024-08-04 15:38:54 -0300

    creando nuevo modelo para onboardingPhysician

---
+++ src/models/PhysicianOnboarding.js
@@ -0,0 +1,78 @@
+import sequelize from "sequelize";
+
+const DataTypes = sequelize.DataTypes;
+
+const model = (sequelize) => {
+  sequelize.define(
+    "PhysicianOnboarding",
+    {
+      id: {
+        autoIncrement: true,
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+      },
+      idPhysician: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        field: "reviews_score",
+        unique: true,
+        references: {
+          model: "user",
+          key: "id",
+        },
+      },
+      genre: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+          model: "cat_genre",
+          key: "id",
+        },
+      },
+      birthDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true,
+        field: "birth_date",
+      },
+      address: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      centerAttention: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        field: "center_attention",
+        references: {
+          model: "cat_center_attention",
+          key: "id",
+        },
+      },
+      specialty: {
+        type: DataTypes.BIGINT,
+        allowNull: true,
+        references: {
+          model: "cat_medical_specialty",
+          key: "id",
+        },
+      },
+      nacionalRegistration: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "nacional_registration",
+      },
+      provincialRegistration: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "provincial_registration",
+      },
+    },
+    {
+      tableName: "physician_onboarding",
+      schema: "public",
+      timestamps: false,
+    }
+  );
+};
+
+export default model;
