407665b4bd7bf516e6797d61285d940c3829baa5
gablon29 <gabriellondero40@gmail.com>
2024-08-04 19:19:07 -0300

    actual

--- src/handlers/onbording/createOnbPhysician.js
+++ src/handlers/onbording/createOnbPhysician.js
@@ -8,7 +8,7 @@ export const createOnbPhysician = async (body, userId) => {
     birthDate,
     address,
     centerAttention,
-    specialty,
+    speciality,
     nacionalRegistration,
     provincialRegistration,
   } = body;
@@ -19,7 +19,7 @@ export const createOnbPhysician = async (body, userId) => {
       birthDate,
       address,
       centerAttention,
-      specialty,
+      speciality,
       nacionalRegistration,
       provincialRegistration,
     });
