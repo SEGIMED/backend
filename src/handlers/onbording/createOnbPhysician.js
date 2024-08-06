204ca600a4e362e3b636bb2dc219c79415fa7d6d
gablon29 <gabriellondero40@gmail.com>
2024-08-06 17:08:29 -0300

    actualizando rama

--- src/handlers/onbording/createOnbPhysician.js
+++ src/handlers/onbording/createOnbPhysician.js
@@ -23,7 +23,37 @@ export const createOnbPhysician = async (body, userId) => {
       nacionalRegistration,
       provincialRegistration,
     });
-    return newOnbPhysician;
+    const [newSpecialty, created] =
+      await models.PhysicianSpecialty.findOrCreate({
+        where: { physician: convertUserid, medicalSpecialty: specialty },
+      });
+
+    const [newMedicalRegistryProvincial, createdProvincial] =
+      await models.PhysicianMedicalRegistry.findOrCreate({
+        where: {
+          physician: convertUserid,
+          registryType: 1,
+          registryId: provincialRegistration,
+        },
+        defaults: { registryId: provincialRegistration },
+      });
+
+    const [newMedicalRegistryNacional, createdNacional] =
+      await PhysicianMedicalRegistry.findOrCreate({
+        where: {
+          physician: convertUserid,
+          registryType: 2,
+          registryId: nacionalRegistration,
+        },
+        defaults: { registryId: nacionalRegistration },
+      });
+
+    return {
+      newOnbPhysician,
+      newMedicalRegistryNacional,
+      newMedicalRegistryProvincial,
+      newSpecialty,
+    };
   } catch (error) {
     throw new SegimedAPIError(500, error.message);
   }
