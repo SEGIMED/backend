8d564f9af3ceead153606877e7bc25a9f0720cb9
gablon29 <gabriellondero40@gmail.com>
2024-08-12 14:17:33 -0300

    actualizando cambios

--- src/realtime_server/webchat/classes/videoCall/roomVideoCall.js
+++ src/realtime_server/webchat/classes/videoCall/roomVideoCall.js
@@ -1,41 +1,38 @@
 import { getDataConsult } from "../../../dbmethods/methodsConsult.js";
 
-export class Room{
-    constructor(){
-        this.key = null;
-        this.users= null;
-        this.id = null;
-        this.offer= null;
-        this.asw = null;
-        this.candidate = [];
+export class Room {
+  constructor() {
+    this.key = null;
+    this.users = null;
+    this.id = null;
+    this.offer = null;
+    this.asw = null;
+    this.candidate = [];
+  }
+
+  async getData(id) {
+    try {
+      const result = await getDataConsult(id);
+
+      if (result) this.assignData(result);
+
+      return this;
+    } catch (error) {
+      console.log(error.message);
+      return error;
     }
+  }
+  assignData(obj) {
+    this.key = obj.users.sort().join("-");
+    this.id = obj.id;
+    this.users = obj.users;
+    this.patient = { ...obj.patient, state: false };
+    this.physician = { ...obj.physician, state: false };
+  }
 
-    async getData(id){
-        try {
-            const result = await getDataConsult(id);
-
-            if(result) this.assignData(result)
-                
-            return this
-
-        } catch (error) {
-            console.log(error.message)
-            return error
-        }
-    }
-    assignData(obj){
-       this.key=obj.users.sort().join("-"); 
-       this.id=obj.id
-       this.users=obj.users
-       this.patient = {...obj.patient,state:false}
-       this.physician = {...obj.physician,state:false}
-    };
-
-    setUserState(id,value){
-        this.patient.id === id ? this.patient.state = value : this.physician.state = value;
-    }
-
-
-
-
+  setUserState(id, value) {
+    this.patient.id === id
+      ? (this.patient.state = value)
+      : (this.physician.state = value);
+  }
 }
