9479c5c2f0e687e50e2e6bcb6499bf999bda2630
gablon29 <gabriellondero40@gmail.com>
2024-08-12 14:21:22 -0300

    actualizando cambios

--- src/realtime_server/webchat/classes/videoCall/roomVideoCall.js
+++ src/realtime_server/webchat/classes/videoCall/roomVideoCall.js
@@ -1,38 +1,41 @@
 import { getDataConsult } from "../../../dbmethods/methodsConsult.js";
 
-export class Room {
-  constructor() {
-    this.key = null;
-    this.users = null;
-    this.id = null;
-    this.offer = null;
-    this.asw = null;
-    this.candidate = [];
-  }
-
-  async getData(id) {
-    try {
-      const result = await getDataConsult(id);
-
-      if (result) this.assignData(result);
-
-      return this;
-    } catch (error) {
-      console.log(error.message);
-      return error;
+export class Room{
+    constructor(){
+        this.key = null;
+        this.users= null;
+        this.id = null;
+        this.offer= null;
+        this.asw = null;
+        this.candidate = [];
     }
-  }
-  assignData(obj) {
-    this.key = obj.users.sort().join("-");
-    this.id = obj.id;
-    this.users = obj.users;
-    this.patient = { ...obj.patient, state: false };
-    this.physician = { ...obj.physician, state: false };
-  }
 
-  setUserState(id, value) {
-    this.patient.id === id
-      ? (this.patient.state = value)
-      : (this.physician.state = value);
-  }
+    async getData(id){
+        try {
+            const result = await getDataConsult(id);
+
+            if(result) this.assignData(result)
+                
+            return this
+
+        } catch (error) {
+            console.log(error.message)
+            return error
+        }
+    }
+    assignData(obj){
+       this.key=obj.users.sort().join("-"); 
+       this.id=obj.id
+       this.users=obj.users
+       this.patient = {...obj.patient,state:false}
+       this.physician = {...obj.physician,state:false}
+    };
+
+    setUserState(id,value){
+        this.patient.id === id ? this.patient.state = value : this.physician.state = value;
+    }
+
+
+
+
 }
--- src/realtime_server/webchat/handlers/videoCallHandler.js
+++ src/realtime_server/webchat/handlers/videoCallHandler.js
@@ -1,66 +1,69 @@
 import ListVideoCall from "../classes/videoCall/listVideoCall.js";
 
-export default (io, socket) => {
-  const createOffer = async (data) => {
-    const dataRoom = await ListVideoCall.findOrCreateRoom(data.id);
-    ListVideoCall.setOfferRoomById(data.id);
-    const { userId } = socket.decoded;
-    const targetID = dataRoom.users.find((id) => id !== userId);
-    if (io.users[targetID]) io.users[targetID].emit("onOffer", data.offer);
-  };
-
-  const createAsw = async (data) => {
-    const dataRoom = await ListVideoCall.findOrCreateRoom(data.id);
-    ListVideoCall.setAswRoomById(data.id);
-    const { userId } = socket.decoded;
-    const targetID = dataRoom.users.find((user) => user.id !== userId);
-    if (io.users[targetID.id]) io.users[targetID.id].emit("onAsw", data.asw);
-  };
-
-  const userState = async (data) => {
-    const { consultId, state } = data;
-    const { userId, role } = socket.decoded;
-    const findRoom = await ListVideoCall.findOrCreateRoom(consultId);
-    let targetId = null;
-    if (role === "Médico") {
-      findRoom.physician.state = state;
-      targetId = findRoom.patient.id;
-    } else {
-      findRoom.patient.state = state;
-      targetId = findRoom.physician.id;
+export default (io,socket) => {
+    const createOffer = async (data) => {
+        const dataRoom = await ListVideoCall.findOrCreateRoom(data.id);
+        ListVideoCall.setOfferRoomById(data.id);
+        const {userId} = socket.decoded
+        const targetID = dataRoom.users.find(id => id !== userId);
+        if(io.users[targetID]) io.users[targetID].emit("onOffer",data.offer);
     }
 
-    if (io.users[targetId]) {
-      io.users[targetId].emit("updateRoom", findRoom);
+    const createAsw = async(data) => {
+            const dataRoom = await ListVideoCall.findOrCreateRoom(data.id);
+            ListVideoCall.setAswRoomById(data.id);
+            const {userId} = socket.decoded
+            const targetID = dataRoom.users.find(user => user.id !== userId);
+            if(io.users[targetID.id]) io.users[targetID.id].emit("onAsw",data.asw);
     }
-    socket.emit("updateRoom", findRoom);
-  };
-  const newCandidate = async (data) => {
-    const dataRoom = await ListVideoCall.findOrCreateRoom(data.id);
-    const candidate = data.candidate;
-    const { userId } = socket.decoded;
-    const targetID = dataRoom.users.find((user) => user.id !== userId);
-    if (io.users[targetID.id]) {
-      io.users[targetID.id].emit("newCandidate", candidate);
-    }
-  };
-  const joinRoom = async (consultId) => {
-    try {
-      if (!consultId) throw new Error("Error, no recibio el id de la consulta");
-      const data = await ListVideoCall.findOrCreateRoom(consultId);
-      const myId = socket.decoded.userId;
-      data.setUserState(myId, true);
-      const target = data.users.find((user) => user.id !== myId);
-      if (io.users[target.id]) {
-        io.users[target.id].emit("dataRoom", data);
-      }
-      socket.emit("dataRoom", data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
 
-  /*
+    const userState = async (data) => {
+        const {consultId,state} = data; 
+        const {userId,role} = socket.decoded
+        const findRoom = await ListVideoCall.findOrCreateRoom(consultId);
+        let targetId = null;
+        if(role==="Médico"){
+            findRoom.physician.state = state;
+            targetId = findRoom.patient.id
+        } else {
+            findRoom.patient.state = state;
+            targetId = findRoom.physician.id
+        }
+
+        if(io.users[targetId]){
+            io.users[targetId].emit("updateRoom",findRoom)
+        }
+        socket.emit("updateRoom",findRoom);
+    }
+    const newCandidate = async(data) => {
+        const dataRoom = await ListVideoCall.findOrCreateRoom(data.id);
+        const candidate = data.candidate;
+        const {userId} = socket.decoded
+        const targetID = dataRoom.users.find(user => user.id !== userId);
+        if(io.users[targetID.id]){
+            io.users[targetID.id].emit("newCandidate",candidate);
+        }
+
+    }
+    const joinRoom = async (consultId) => {
+
+        try {
+            if(!consultId) throw new Error('Error, no recibio el id de la consulta');
+            const data = await ListVideoCall.findOrCreateRoom(consultId);
+            const myId = socket.decoded.userId;
+            data.setUserState(myId,true);
+            const target = data.users.find(user => user.id !== myId);
+            if(io.users[target.id]){
+                io.users[target.id].emit('dataRoom',data);
+            }
+            socket.emit('dataRoom',data)
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    }
+    
+    /*
         Modificar. 
 
         1- Evento para entrar a la sala.
@@ -70,9 +73,9 @@ export default (io, socket) => {
         5- Evento para contestar la llamada (asw)
         6- Evento para finalizar. ( Limpiar la data local);
     */
-  socket.on("joinRoom", joinRoom);
-  socket.on("sendAsw", createAsw);
-  socket.on("sendOffer", createOffer);
-  socket.on("newCandidate", newCandidate);
-  socket.on("userState", userState);
-};
+    socket.on("joinRoom", joinRoom);
+    socket.on("sendAsw",createAsw);
+    socket.on('sendOffer',createOffer);
+    socket.on('newCandidate',newCandidate);
+    socket.on('userState',userState);
+}
