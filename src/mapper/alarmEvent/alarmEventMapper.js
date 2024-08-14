9bffecea59b819312cf4e4f23837e14eacd98422
Santiago Paz <santipaz19.02@gmail.com>
2024-08-14 17:09:54 -0300

    alarm fix

--- src/mapper/alarmEvent/alarmEventMapper.js
+++ src/mapper/alarmEvent/alarmEventMapper.js
@@ -1,23 +1,39 @@
-export const mapquestionsPriority = (questionsPriority) =>{
-    
-    let counter=0
-    for(let i =0; i< questionsPriority.length; i++){
-        let [questionNumber, _] = questionsPriority[i].split(':');
-   
-        if(counter!= questionNumber){
-            questionsPriority.splice(counter,0,counter.toString()+':0')
-        }
-        counter++
-    }
-        
+export const mapquestionsPriority = (questionsPriority) => {
+    // Si questionsPriority es null o undefined, inicializamos con un array vacío
+    if (!questionsPriority) return [];
 
-    const mappAnswer = questionsPriority.map(pregunta => {
-        let [questionNumber,numberPriority] = pregunta.split(':');
-        
-        if(numberPriority==="") numberPriority='0'
-        const wordPriority = numberPriority === '1' ? 'Alta' : numberPriority === '2' ? 'Media' : numberPriority === '3'? 'Baja' : numberPriority === '0' ? '0' : 'Prioridad inválida';
-        return `${questionNumber}: ${wordPriority}` 
-        
-      });
-    return  mappAnswer
-}
+    let counter = 0;
+    let mappedQuestions = [];
+
+    for (let i = 0; i < questionsPriority.length; i++) {
+        let [questionNumber, _] = questionsPriority[i].split(':');
+
+        // Si hay una brecha en los números de las preguntas, inserta la pregunta faltante con prioridad 0
+        while (counter != questionNumber) {
+            mappedQuestions.push(`${counter}: 0`);
+            counter++;
+        }
+
+        counter++;
+        mappedQuestions.push(questionsPriority[i]);
+    }
+
+    // Si quedan preguntas faltantes al final, se añaden con prioridad 0
+    while (counter < questionsPriority.length) {
+        mappedQuestions.push(`${counter}: 0`);
+        counter++;
+    }
+
+    const mappedAnswer = mappedQuestions.map(pregunta => {
+        let [questionNumber, numberPriority] = pregunta.split(':');
+
+        if (numberPriority === "") numberPriority = '0';
+        const wordPriority = numberPriority === '1' ? 'Alta' :
+            numberPriority === '2' ? 'Media' :
+                numberPriority === '3' ? 'Baja' :
+                    numberPriority === '0' ? '0' : 'Prioridad inválida';
+        return `${questionNumber}: ${wordPriority}`;
+    });
+
+    return mappedAnswer;
+};
