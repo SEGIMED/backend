19a89438e74be52b2b3b6396bee83fe998c91c7d
gabri <gabriellondero40@gmail.com>
2024-08-18 10:52:13 -0300

    fusionando cambios

---
+++ src/handlers/physicianHandlers/orders/getOrdersById.js
@@ -0,0 +1,10 @@
+import SegimedAPIError from "../../../error/SegimedAPIError.js";
+import models from "../../../databaseConfig.js"
+
+const getOrdersByIdHandlerPhysician = async (userId) => {
+    try {
+        const registerOrders = await 
+    } catch (error) {
+        throw new SegimedAPIError("Error al procesar la informacion: ", error)
+    }
+};
