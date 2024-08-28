8027c5ef83647ee00ba5d69b9877c788feb54dcc
gablon29 <gabriellondero40@gmail.com>
2024-08-28 16:15:45 -0300

    modificando el patch de bakground para que se pueda modificar todos los campos salvo el de id

--- src/handlers/Backgrounds/updateBackgroundsHandler.js
+++ src/handlers/Backgrounds/updateBackgroundsHandler.js
@@ -1,9 +1,28 @@
-import {Backgrounds} from "../../databaseConfig.js";
+import { Backgrounds } from "../../databaseConfig.js";
 import SegimedAPIError from "../../error/SegimedAPIError.js";
 
-const updateBackgroundsHandler = async (body) => {
-    const {
-        id,
+const updateBackgroundsHandler = async (id, body) => {
+  if (!id) {
+    throw new SegimedAPIError("El id es requerido.", 400);
+  }
+  const {
+    surgicalBackground,
+    pathologicBackground,
+    nonPathologicBackground,
+    familyBackground,
+    pediatricBackground,
+    pharmacologicalBackground,
+    vaccinationBackground,
+    allergicBackground,
+    timestamp,
+    medicalEvent,
+    appointmentScheduling,
+    patient,
+  } = body;
+
+  try {
+    const updatedBackground = await Backgrounds.update(
+      {
         surgicalBackground,
         pathologicBackground,
         nonPathologicBackground,
@@ -11,33 +30,24 @@ const updateBackgroundsHandler = async (body) => {
         pediatricBackground,
         pharmacologicalBackground,
         vaccinationBackground,
-        allergicBackground
-    } = body
-
-    try {
-        const updatedBackground = await Backgrounds.update(
-            {
-                surgicalBackground,
-                pathologicBackground,
-                nonPathologicBackground,
-                familyBackground,
-                pediatricBackground,
-                pharmacologicalBackground,
-                vaccinationBackground,
-                allergicBackground,
-            },
-            {
-                where: {
-                    id: id
-                },
-                returning: true,
-                plain: true
-            }
-        )
-        return updatedBackground[1]
-    } catch (error) {
-        throw new SegimedAPIError('Hubo un error durante el proceso.', 500)
-    }
+        allergicBackground,
+        timestamp,
+        medicalEvent,
+        appointmentScheduling,
+        patient,
+      },
+      {
+        where: {
+          id: id,
+        },
+        returning: true,
+        plain: true,
+      }
+    );
+    return updatedBackground[1];
+  } catch (error) {
+    throw new SegimedAPIError("Hubo un error durante el proceso.", 500);
+  }
 };
 
 export default updateBackgroundsHandler;
