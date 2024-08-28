6bf16a5f456cf594034c3fb0c12a8b47a6a96e53
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-08-28 16:20:50 -0300

    mediaction prescription duplicated

--- src/controllers/drugPrescription/drugPrescriptionHelper.js
+++ src/controllers/drugPrescription/drugPrescriptionHelper.js
@@ -1,50 +1,77 @@
+import models from "../../databaseConfig.js";
 import createDrugPrescriptionHandler from "../../handlers/drugPrescription/createDrugPrescriptionHandler.js";
 import drugCreationHandler from "../../handlers/drugPrescription/drugCreationHandler.js";
+import updateDrugPrescriptionHandler from "../../handlers/drugPrescription/updateDrugPrescriptionHandler.js";
 import validateDrugCreationData from "../../validations/validateDrugCreation.js";
 import { validateDrugPrescriptionInput } from "../../validations/validateDrugPrescriptionInput.js";
 
 export const createDrugPrescriptions = async (prescriptions, transaction) => {
-    try {
-        if (!Array.isArray(prescriptions)) {
-            throw new Error("El cuerpo de la solicitud debe contener un array de medicamentos.");
-          }
-        
-          for (const prescription of prescriptions) {
-            validateDrugPrescriptionInput(prescription);
-            validateDrugCreationData(prescription.drugCreation);
-        
-            const {
-              drugDetailPresentationId,
-              commercialNameDrugId,
-              drugCreation,
-              prescriptionCreation,
-            } = prescription;
-        
-            let drugDetailId = drugDetailPresentationId;
-            let commercialNameId = commercialNameDrugId;
-        
-            if (!drugDetailId) {
-              const createdDrugDetail = await drugCreationHandler(
-                drugCreation,
-                transaction
-              );
-        
-              drugDetailId = createdDrugDetail.id;
-              commercialNameId = createdDrugDetail.commercialNameDrugId;
-            }
-        
-            await createDrugPrescriptionHandler(
-              {
-                ...prescriptionCreation,
-                drugDetailPresentationId: drugDetailId,
-                commercialNameDrugId: commercialNameId,
-              },
-              transaction
-            );
-          }
-          return "Medicamentos creados con éxito"
-    } catch (error) {
-        throw new Error(error);
-        
+  try {
+    if (!Array.isArray(prescriptions)) {
+      throw new Error(
+        "El cuerpo de la solicitud debe contener un array de medicamentos."
+      );
     }
-  };
+
+    for (const prescription of prescriptions) {
+      validateDrugPrescriptionInput(prescription);
+      validateDrugCreationData(prescription.drugCreation);
+
+      const {
+        drugDetailPresentationId,
+        commercialNameDrugId,
+        drugCreation,
+        prescriptionCreation,
+      } = prescription;
+      const { patientId } = prescription.prescriptionCreation;
+      let drugDetailId = drugDetailPresentationId;
+      let commercialNameId = commercialNameDrugId;
+
+      if (!drugDetailId) {
+        const createdDrugDetail = await drugCreationHandler(
+          drugCreation,
+          transaction
+        );
+
+        drugDetailId = createdDrugDetail.id;
+        commercialNameId = createdDrugDetail.commercialNameDrugId;
+      }
+
+      //Verificación pa ver si ya existe una prescripción para el paciente
+
+      const existingPrescription = await models.MedicationPrescription.findOne({
+        where: {
+          patientId,
+        },
+        include: {
+          model: models.PrescriptionModificationsHistory,
+          as: "medicationPrescription",
+          where: {
+            drugDetailPresentationId: drugDetailId,
+          },
+        },
+      });
+      if (existingPrescription && existingPrescription.active && !existingPrescription.deleted) {
+        await updateDrugPrescriptionHandler(
+          {
+            medicationPrescriptionId: existingPrescription.id,
+            ...prescriptionCreation,
+          },
+          transaction
+        );
+      } else {
+        await createDrugPrescriptionHandler(
+          {
+            ...prescriptionCreation,
+            drugDetailPresentationId: drugDetailId,
+            commercialNameDrugId: commercialNameId,
+          },
+          transaction
+        );
+      }
+    }
+    return "Medicamentos creados con éxito";
+  } catch (error) {
+    throw new Error(error);
+  }
+};
