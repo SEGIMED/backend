30daf65a9378f5d2dbea12ca58894d3580a3e0a4
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-08-31 13:32:28 -0300

    update htp fix

--- src/handlers/patient/updatePatientHpGroupHandler.js
+++ src/handlers/patient/updatePatientHpGroupHandler.js
@@ -1,27 +1,45 @@
-import {PatientPulmonaryHypertensionGroup} from "../../databaseConfig.js";
+import moment from "moment";
+import { PatientPulmonaryHypertensionGroup } from "../../databaseConfig.js";
 import SegimedAPIError from "../../error/SegimedAPIError.js";
-
-
+const TZ = process.env.TZ;
 const updatePatientHpGroupHandler = async (body) => {
-    const {patientId} = body
+  const { patientId, hpGroups, physicianId } = body;
 
-    try {
-        const updateHpGroup = await PatientPulmonaryHypertensionGroup.update(
-            {
-                group: body.hpGroupId
-            },
-            {
-                where: {
-                    patient: patientId
-                },
-                returning: true,
-                plain: true
-            }
-        )
-        return updateHpGroup[1]
-    } catch (error) {
-        throw new SegimedAPIError('Hubo un error durante el proceso de actualización.', 500)
+  try {
+    const existingHPgroup = await PatientPulmonaryHypertensionGroup.findAll({
+      where: {
+        patient: patientId,
+      },
+    });
+
+    const existingIds = existingHPgroup.map((group) => group.group);
+    const toAdd = hpGroups.filter((id) => !existingIds.includes(id));
+    const toRemove = existingIds.filter((id) => !hpGroups.includes(id));
+
+    if (toAdd.length > 0) {
+      const newGroups = toAdd.map((group) => ({
+        patient: patientId,
+        group,
+        physician: physicianId,
+        timestamp: moment().tz(TZ).toISOString(),
+      }));
+      await PatientPulmonaryHypertensionGroup.bulkCreate(newGroups);
     }
+    if (toRemove.length > 0) {
+      await PatientPulmonaryHypertensionGroup.destroy({
+        where: {
+          patient: patientId,
+          group: toRemove,
+        },
+      });
+    }
+    return "HTP actualizado correctamente";
+  } catch (error) {
+    throw new SegimedAPIError(
+      "Hubo un error durante el proceso de actualización.",
+      500
+    );
+  }
 };
 
 export default updatePatientHpGroupHandler;
