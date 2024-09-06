e6a94c59238298bb06fae0b4b71555d2368d41c5
Andre <andre021297@gmail.com>
2024-09-06 11:01:00 -0500

    add logs to login

--- src/controllers/user/userLoginController.js
+++ src/controllers/user/userLoginController.js
@@ -1,17 +1,16 @@
 import userLoginHandler from "../../handlers/user/userLoginHandler.js";
 
-
 const userLoginController = async (req, res) => {
-    try {
-        const loginResponse = await userLoginHandler(req.body);
-        return res.status(200).json({
-            msg: 'Iniciaste sesion correctamente.',
-            authenticationDetails: loginResponse,
-        });
-
-    } catch (error) {
-        return res.status(error.errorCode).json({error: error.message});
-    }
-}
+  try {
+    const loginResponse = await userLoginHandler(req.body);
+    return res.status(200).json({
+      msg: "Iniciaste sesion correctamente.",
+      authenticationDetails: loginResponse,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(error.errorCode).json({ error: error.message });
+  }
+};
 
 export default userLoginController;
