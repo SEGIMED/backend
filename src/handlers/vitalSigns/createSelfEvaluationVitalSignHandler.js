b6f1af5bc7ec0d6052403d285ee9b76dabad3472
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-09-12 11:30:54 -0300

    glycemia selfEvaluationEvent

--- src/handlers/vitalSigns/createSelfEvaluationVitalSignHandler.js
+++
@@ -1,9 +0,0 @@
-const createSelfEvaluationVitalSignHandler = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        throw new Error("Ocurrio un error al guardar los datos: " + error.message);
-        
-    }
-}
-export default createSelfEvaluationVitalSignHandler
